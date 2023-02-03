06273bd67c98b7ad44dca35e6990d75ddf42f656
Dikshant Tyagi <84742022+Dikshant130@users.noreply.github.com>
2023-02-03 23:23:03 +0530

    Add files via upload

---
+++ src/components/Brands/Pricing.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Button } from '../../globalStyles';
+import { AiFillThunderbolt } from 'react-icons/ai';
+import { GiCrystalBars } from 'react-icons/gi';
+import { GiCutDiamond, GiRock } from 'react-icons/gi';
+import { GiFloatingCrystal } from 'react-icons/gi';
+import { IconContext } from 'react-icons/lib';
+import {
+  PricingSection,
+  PricingWrapper,
+  PricingHeading,
+  PricingContainer,
+  PricingCard,
+  PricingCardInfo,
+  PricingCardIcon,
+  PricingCardPlan,
+  PricingCardCost,
+  PricingCardLength,
+} from './Pricing.elements';
+
+ const Pricing = () => {
+  return (
+    <IconContext.Provider value={{ color: '#a9b3c1', size: 64 }}>
+      <PricingSection>
+        <PricingWrapper>
+          <PricingHeading>Brands We Deal In</PricingHeading>
+          <PricingContainer>
+            <PricingCard to='/sign-up'>
+              <PricingCardInfo>
+                <PricingCardIcon>
+                  <GiRock />
+                </PricingCardIcon>
+                <PricingCardPlan>Sinmag</PricingCardPlan>
+                <PricingCardCost>Est </PricingCardCost>
+                <PricingCardLength>per month</PricingCardLength>
+                <Button primary>Choose Plan</Button>
+              </PricingCardInfo>
+            </PricingCard>
+            <PricingCard to='/sign-up'>
+              <PricingCardInfo>
+                <PricingCardIcon>
+                  <GiCrystalBars />
+                </PricingCardIcon>
+                <PricingCardPlan>ABC</PricingCardPlan>
+                <PricingCardCost>$299.99</PricingCardCost>
+                <PricingCardLength>per month</PricingCardLength>
+                <Button primary>Choose Plan</Button>
+              </PricingCardInfo>
+            </PricingCard>
+            <PricingCard to='/sign-up'>
+              <PricingCardInfo>
+                <PricingCardIcon>
+                  <GiCutDiamond />
+                </PricingCardIcon>
+                <PricingCardPlan>XYZ</PricingCardPlan>
+                <PricingCardCost>$999.99</PricingCardCost>
+                <PricingCardLength>per month</PricingCardLength>
+                <Button primary>Choose Plan</Button>
+              </PricingCardInfo>
+            </PricingCard>
+          </PricingContainer>
+        </PricingWrapper>
+      </PricingSection>
+    </IconContext.Provider>
+  );
+}
+
+export default Pricing
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Footer } from './Footer/Footer';
+export { default as About } from './About/About';
+export { default as Contact } from './Contact/Contact';
+export { default as Pricing } from './Brands/Pricing';
